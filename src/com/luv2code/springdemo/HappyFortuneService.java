585b1aa2686d2b92153366fa6f9d2bc7d25d03a6
Rastisslav <bolibruch2000@gmail.com>
2022-06-01 18:52:19 +0200

    did task

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,6 +1,9 @@
 package com.luv2code.springdemo;
+import java.util.Random;
 
 public class HappyFortuneService implements FortuneService{
+
+    //String[] fortunes = {"Today is ur lucky day","Today is ur very lucky day","Today is ur luckiest day"};
 
     @Override
     public String getFortune() {
