b5e017d72869cea78725c716e8e5a115b95e5634
Rastisslav <bolibruch2000@gmail.com>
2022-06-02 17:31:56 +0200

    lifeCyclePractice

--- src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
+++ src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
@@ -2,28 +2,26 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class BeanScopeDemoApp {
+public class BeanLifeCycleDemoApp {
 
     public static void main(String[] args) {
 
         // load the spring configuration file
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+                new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
 
         // retrieve bean from spring container
         Coach theCoach = context.getBean("myHockeyCoach", Coach.class);
 
-        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+        Coach alphaCoach = context.getBean("myHockeyCoach", Coach.class);
 
-        // check if they are the same
+        System.out.println(theCoach.getDailyWorkout());
+
         boolean result = (theCoach == alphaCoach);
 
         // print out the results
         System.out.println("\nPointing to the same object: " + result);
 
-        System.out.println("\nMemory location for theCoach: " + theCoach);
-
-        System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
 
         // close the context
         context.close();
--- src/com/luv2code/springdemo/HockeyCoach.java
+++ src/com/luv2code/springdemo/HockeyCoach.java
@@ -1,6 +1,12 @@
 package com.luv2code.springdemo;
 
 public class HockeyCoach implements Coach {
+
+    private FortuneService fortuneService;
+    public HockeyCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout(){
         return "Run around";
